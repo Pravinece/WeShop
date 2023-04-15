c6914d519a02a609dfa430b8f2c48427fad6b565
Pravinece <130856052+Pravinece@users.noreply.github.com>
2023-04-15 21:45:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-bars');
+let topnav = document.querySelector('.topnav');
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .topnav a');
+
+window.onscroll = () =>{
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .topnav a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
